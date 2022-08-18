79f5cf35abe21ddb6494441e8d69df135ce7dda9
coke-official <cokeofficial1886@gmail.com>
2022-08-18 17:47:52 +0900

    first commit

---
+++ elpa/el-get-20211224.959/el-get-pkg.el
@@ -0,0 +1,10 @@
+(define-package "el-get" "20211224.959" "Manage the external elisp bits and pieces you depend upon" 'nil :commit "bf3dba444dcd240b8cb358a0850c8c5a92606134" :authors
+  '(("Dimitri Fontaine" . "dim@tapoueh.org"))
+  :maintainer
+  '("Dimitri Fontaine" . "dim@tapoueh.org")
+  :keywords
+  '("emacs" "package" "elisp" "install" "elpa" "git" "git-svn" "bzr" "cvs" "svn" "darcs" "hg" "apt-get" "fink" "pacman" "http" "http-tar" "emacswiki")
+  :url "http://www.emacswiki.org/emacs/el-get")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ var/prescient-save.el
@@ -0,0 +1,2 @@
+
+(funcall prescient-cache-callback :version '5 :history '#s(hash-table size 65 test equal rehash-size 1.5 rehash-threshold 0.8125 data ("init.el" 1 "leaf-tree-mode" 3 "leaf-convert-region-replace" 2 "tetris" 0)) :frequency '#s(hash-table size 65 test equal rehash-size 1.5 rehash-threshold 0.8125 data ("init.el" 12.680604348226202 "leaf-tree-mode" 0.9502058312852957 "leaf-convert-region-replace" 2.86780710379999 "tetris" 0.997)) :serial-number '18)
